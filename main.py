c0e716de0ef65ed69ef9a5d301bce2977e9cd493
MatinQurban <matinqurban@gmail.com>
2022-10-19 01:13:19 -0700

    Erase config and beautify

--- main.py
+++ main.py
@@ -14,9 +14,10 @@ from loguru import logger
 
 if __name__ == "__main__":
 
+    #finding odrive
     odrv0 = odrive.find_any()
 
-    #test function
+    #test function -- getting serial number from the located odrive
     odrv0SerNum = str(hex(odrv0.serial_number)).upper()
     print(odrv0SerNum.replace('0X',''))
 
@@ -29,15 +30,26 @@ if __name__ == "__main__":
     # #test function
     # odrv1SerNum = str(hex(odrv1.serial_number)).upper()
     # print(odrv1SerNum.replace('0X',''))
-    # #===================================
-    # logger.debug("Do you want to save/erase/reboot? [Y/N]")
-    # SERcmd = input().upper()
 
-    # if SERcmd == 'Y':
+    #===================Reset=========================
+    #If there were errors in the previous cycle, erase config would clear errors so you could start over
+    logger.debug("Do you want to erase? [Y/N]")
+    ERcmd = input()
 
+    if ERcmd.upper() == 'Y':
+        try:
+            odrv0.erase_configuration()
 
+        except fibre.libfibre.ObjectLostError:
+            pass
 
+        odrv0 = odrive.find_any()
 
+    logger.debug("Manual configuration erased.")
+    #================================================
+
+    
+    #=============ODRIVE CONFIGURATION===============
     odrv0.config.enable_brake_resistor = False
     odrv0.config.brake_resistance = 0.0
     odrv0.config.dc_bus_undervoltage_trip_level = 8.0
@@ -45,7 +57,9 @@ if __name__ == "__main__":
     odrv0.config.dc_max_positive_current = 120.0
     odrv0.config.dc_max_negative_current = -20.0
     odrv0.config.max_regen_current = 0
+    #================================================
 
+    #=============MOTOR CONFIGURATION================
     odrv0.axis0.motor.config.pole_pairs = 7
     odrv0.axis0.motor.config.resistance_calib_max_voltage = 3.0
     odrv0.axis0.motor.config.motor_type = MOTOR_TYPE_HIGH_CURRENT
@@ -53,16 +67,21 @@ if __name__ == "__main__":
     odrv0.axis0.motor.config.current_control_bandwidth = 2000
     odrv0.axis0.motor.config.current_lim = 100
     odrv0.axis0.motor.config.torque_constant = 8.27 / 473
+    #================================================
 
+    #================ENCODER CONFIGURATION====================
+    #Can use ENCODER_MODE_HALL as found in odrive.enums
     odrv0.axis0.encoder.config.mode = ENCODER_MODE_HALL
     odrv0.axis0.encoder.config.cpr = 42
     #using an encoder with an index pin to allow pre-calibration of the encoder and encoder index search
+    #ours has index pins ; set it to true
     odrv0.axis0.encoder.config.use_index = False
-    #Change from true to false
+    #Changed from true to false
     odrv0.axis0.encoder.config.ignore_illegal_hall_state = False
-
     odrv0.axis0.encoder.config.calib_scan_distance = 150
     odrv0.axis0.encoder.config.bandwidth = 500
+    #=========================================================
+
 
     odrv0.axis0.config.calibration_lockin.current = 20
     odrv0.axis0.config.calibration_lockin.ramp_time = 0.4
@@ -91,6 +110,7 @@ if __name__ == "__main__":
     except fibre.libfibre.ObjectLostError:
         pass
     logger.debug("Manual configuration saved.")
-
+    #After every save_configuration / erase_configuration / reboot we have to find odrive again.
     odrv0 = odrive.find_any()
-    print(odrv0.serial_number)
+
+

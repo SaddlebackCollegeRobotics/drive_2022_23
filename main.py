3b26629867cd3cceaad644a6da5b3d92cb992dd6
MatinQurban <matinqurban@gmail.com>
2022-10-19 00:29:26 -0700

    save/erase/reboot pretest

--- main.py
+++ main.py
@@ -8,18 +8,23 @@ import sys
 import time
 import odrive
 from odrive.enums import *
-#import fibre.libfibre
+import fibre.libfibre
 #from enum import IntEnum
 from loguru import logger
 
 if __name__ == "__main__":
 
-    #initialize odrv0
     odrv0 = odrive.find_any()
 
     #test function
     odrv0SerNum = str(hex(odrv0.serial_number)).upper()
     print(odrv0SerNum.replace('0X',''))
+
+    logger.debug("Do you want to save/erase/reboot? [Y/N]")
+    SERcmd = input().upper()
+
+    if SERcmd == 'Y':
+        
 
     odrv0.config.enable_brake_resistor = False
     odrv0.config.brake_resistance = 0.0
@@ -30,18 +35,19 @@ if __name__ == "__main__":
     odrv0.config.max_regen_current = 0
 
     odrv0.axis0.motor.config.pole_pairs = 7
-    odrv0.axis0.motor.config.calibration_current = 5
     odrv0.axis0.motor.config.resistance_calib_max_voltage = 3.0
     odrv0.axis0.motor.config.motor_type = MOTOR_TYPE_HIGH_CURRENT
-    odrv0.axis0.motor.config.current_lim = 100
-    odrv0.axis0.motor.config.current_control_bandwidth = 2000
     odrv0.axis0.motor.config.requested_current_range = 100
+    odrv0.axis0.motor.config.current_control_bandwidth = 2000
+    odrv0.axis0.motor.config.current_lim = 100
     odrv0.axis0.motor.config.torque_constant = 8.27 / 473
 
     odrv0.axis0.encoder.config.mode = ENCODER_MODE_HALL
     odrv0.axis0.encoder.config.cpr = 42
+    #using an encoder with an index pin to allow pre-calibration of the encoder and encoder index search
     odrv0.axis0.encoder.config.use_index = False
-    odrv0.axis0.encoder.config.ignore_illegal_hall_state = True
+    #Change from true to false
+    odrv0.axis0.encoder.config.ignore_illegal_hall_state = False
 
     odrv0.axis0.encoder.config.calib_scan_distance = 150
     odrv0.axis0.encoder.config.bandwidth = 500
@@ -66,11 +72,13 @@ if __name__ == "__main__":
     odrv0.axis0.controller.config.control_mode = CONTROL_MODE_VELOCITY_CONTROL
 
     # saving the new configuration
-    # logger.debug("Saving manual configuration and rebooting...")
-    # try:
-    #     odrv0.save_configuration()
-    # except fibre.libfibre.ObjectLostError:
-    #     pass
-    # logger.debug("Manual configuration saved.")
+    logger.debug("Saving manual configuration and rebooting...")
+    try:
+        odrv0.save_configuration()
 
-    # self._find_odrive()
+    except fibre.libfibre.ObjectLostError:
+        pass
+    logger.debug("Manual configuration saved.")
+
+    odrv0 = odrive.find_any()
+    print(odrv0.serial_number)

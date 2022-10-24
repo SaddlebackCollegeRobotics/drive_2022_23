cf198d559277da25af48e5247b44024948b3a4c2
MatinQurban <matinqurban@gmail.com>
2022-10-23 18:20:02 -0700

    finalized find_serial

--- find_serial.py
+++ find_serial.py
@@ -1,13 +1,16 @@
 import subprocess
 
-#if __name__ == "__main__":
 
 def get_all_odrives():
+
+    odrivesSerNum = []
+
     usbDevices = str(subprocess.run('lsusb', capture_output=True).stdout).split('\\n')
 
     for device in usbDevices:
         if "ODrive" in device:
             serialNumber = device.split("Serial: ")[1]
-            print(serialNumber)
+            odrivesSerNum.append(serialNumber)
 
+    return odrivesSerNum
         

305c8944cc9112974993927f6101a4a391a0fdf3
max9001 <111012399+max9001@users.noreply.github.com>
2022-10-26 15:51:27 -0700

    fixed subprocess thingy

--- find_serial.py
+++ find_serial.py
@@ -19,16 +19,24 @@ import subprocess
 #  uses lsusb in the terminal, captures the output, and parses through to obtain 
 #  the serial numbers. This is turned into a list and returned.
 #==========================================================
-def get_all_odrives():
+
+
+
+
+
+#def get_all_odrives():
+if __name__ == "__main__":
 
     odrivesSerNum = []
 
-    usbDevices = str(subprocess.run('lsusb', capture_output=True).stdout).split('\\n')
+    usbDevices = str(subprocess.run(['lsusb', '-v'], capture_output=True).stdout).split('\\n')
 
     for device in usbDevices:
         if "ODrive" in device:
             serialNumber = device.split("Serial: ")[1]
             odrivesSerNum.append(serialNumber)
 
-    return odrivesSerNum
+    print("yo it's lilz")
+
+    print(usbDevices)
         

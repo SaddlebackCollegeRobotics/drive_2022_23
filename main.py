15bfb722e152fa70a5bbea3a6f666689c055eb60
Jasper <49565505+wluxie@users.noreply.github.com>
2023-01-09 23:20:47 -0800

    Test Drift

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ import subprocess
 
 
 AXIS_DEADZONE = 0.3 # Deadzone is 0 to 1 | Note: axis value will be 0 until you move past the deadzone
+MIN_SPEED = -20
+MAX_SPEED = 20
+CREMENT = 5
 
 
 
@@ -25,6 +28,8 @@ def eventHandler(odrv_0, odrv_1=None):
     gmi.run_event_loop(buttonDownEvents, buttonUpEvents, hatEvents, connectionEvents)   # Async loop to handle gamepad button events
 
     speed = 5
+    rampLVel, rampRVel = 0, 0
+    rampVel = [rampLVel, None, rampRVel]
 
     while True:
         odrv0 = odrive.find_any(serial_number=odrv_0)       # Get odrive object
@@ -41,44 +46,37 @@ def eventHandler(odrv_0, odrv_1=None):
         if gmi.getButtonValue(gp, 1):
             ...
 
+        velocity = ls_y * speed
+        rampVel[int(rs_x) + 1] = (speed * abs(rs_x)) / 1.5
+            
+
         # Left Stick, Forward/Backward Movement
         if l2 > 0 and odrv_1:
-            print("Left Stick: ", ls_x, ls_y)
+            print("Left Stick: ", ls_x, ls_y, "\tRight Stick: ", rs_x, rs_y)
             odrv0.axis0.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
             odrv0.axis1.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
-            odrv0.axis0.controller.input_vel = int(ls_y * speed)
-            odrv0.axis1.controller.input_vel = int(ls_y * speed)
+            odrv0.axis0.controller.input_vel = int(velocity + rampLVel)
+            odrv0.axis1.controller.input_vel = int(velocity + rampLVel)
             odrv1.axis0.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
             odrv1.axis1.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
-            odrv1.axis0.controller.input_vel = int(ls_y * speed)
-            odrv1.axis1.controller.input_vel = int(ls_y * speed)
+            odrv1.axis0.controller.input_vel = int(velocity + rampRVel)
+            odrv1.axis1.controller.input_vel = int(velocity + rampRVel)
 
         # Stop all motors if no analog input
-        if (r2 > 0 or ls_y == 0) and odrv_1:
+        if (ls_y == 0 and ls_x == 0) and odrv_1:
             odrv0.axis0.requested_state = AXIS_STATE_IDLE
             odrv0.axis1.requested_state = AXIS_STATE_IDLE
             odrv1.axis0.requested_state = AXIS_STATE_IDLE
             odrv1.axis1.requested_state = AXIS_STATE_IDLE
 
         # Ramp up/down input
-        if hat_x < 0 and speed != -20:
-            speed -= 5
+        if hat_x < 0 and speed != MIN_SPEED:
+            speed -= CREMENT
             print("Current speed: ", speed)
-        if hat_x > 0 and speed != 20:
-            speed += 5
+        if hat_x > 0 and speed != MAX_SPEED:
+            speed += CREMENT
             print("Current speed: ", speed)
             
-        # Right Stick, Left/Right Movement
-        if rs_x > 0 and odrv1:
-            odrv1.axis0.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
-            odrv1.axis1.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
-            odrv1.axis0.controller.input_vel += int(rs_x * 10)
-            odrv1.axis1.controller.input_vel += int(rs_x * 10)
-        elif rs_x < 0 and odrv1:
-            odrv0.axis0.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
-            odrv0.axis1.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
-            odrv0.axis0.controller.input_vel += int(rs_x * 10)
-            odrv0.axis1.controller.input_vel += int(rs_x * 10)
 
 # ====================================================================================================
         # TODO: Arm control

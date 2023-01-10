478986a8fe98b2e8c8262856d062d78d649afc9b
jasper <49565505+wluxie@users.noreply.github.com>
2023-01-09 21:50:51 -0800

    Clean up src code

--- main.py
+++ main.py
@@ -1,128 +1,7 @@
+from callback_api import *
 from src.odrives.calibrate import *
 import src.controller.gamepad_input as gmi
 import subprocess
-
-
-
-def get_all_odrives():
-
-    odrivesSerialList = []
-
-    # This will pull all devices connected to the computer
-    usbDevices = str(subprocess.run(['lsusb', '-v'], capture_output=True).stdout).split('\\n')
-
-    odriveFound = False
-
-    # This will iterate line by line through the usbDevices[]
-    for line in usbDevices:
-
-        if "ODrive" in line:
-            odriveFound = True
-            print(line)
-
-        # This will pull any device with a serial number 
-        if odriveFound and "Serial" in line:
-            odrivesSerialList.append(line[28:].strip())
-            #odrivesSerialList = list(filter(None, odrivesSerialList))
-            odriveFound = False
-
-    return odrivesSerialList
-
-
-
-def north():
-    print("North")
-
-def west():
-    print("West")
-
-def south():
-    print("South")
-    gmi.rumbleAll(0, 0.3, 100)
-
-def east():
-    print("East")
-
-def share():
-    print("Share")
-
-def options():
-    print("Options")
-
-def home():
-    print("Home")
-
-def l1():
-    print("L1")
-
-def r1():
-    print("R1")
-
-def l3():
-    print("L3")
-
-def r3():
-    print("R3")
-
-
-# Button UP callbacks
-def northUp():
-    print("North Up")
-
-def westUp():
-    print("West Up")
-
-def southUp():
-    print("South Up")
-
-def eastUp():
-    print("East Up")
-
-def shareUp():
-    print("Share Up")
-
-def optionsUp():
-    print("Options Up")
-
-def homeUp():
-    print("Home Up")
-
-def l1Up():
-    print("L1 Up")
-
-def r1Up():
-    print("R1 Up")
-
-def l3Up():
-    print("L3 Up")
-
-def r3Up():
-    print("R3 Up")
-
-
-# Hat callbacks
-def hatNorth():
-    print("Hat North")
-
-def hatSouth():
-    print("Hat South")
-
-def hatWest():
-    print("Hat West")
-
-def hatEast():
-    print("Hat East")
-
-def hatCentered():
-    print("Hat Centered")
-
-
-# Connection callbacks
-def onGamepadConnect():
-    ...
-
-def onGamepadDisconnect():
-    ...
 
 
 
@@ -175,7 +54,7 @@ def eventHandler(odrv_0, odrv_1=None):
             odrv1.axis1.controller.input_vel = int(ls_y * speed)
 
         # Stop all motors if no analog input
-        if r2 > 0 and odrv_1:
+        if (r2 > 0 or ls_y == 0) and odrv_1:
             odrv0.axis0.requested_state = AXIS_STATE_IDLE
             odrv0.axis1.requested_state = AXIS_STATE_IDLE
             odrv1.axis0.requested_state = AXIS_STATE_IDLE
@@ -189,21 +68,19 @@ def eventHandler(odrv_0, odrv_1=None):
             speed += 5
             print("Current speed: ", speed)
             
-
-
-# ====================================================================================================
         # Right Stick, Left/Right Movement
         # if rs_x > 0 and odrv1:
         #     odrv1.axis0.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
         #     odrv1.axis1.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
         #     odrv1.axis0.controller.input_vel += int(rs_x * 10)
         #     odrv1.axis1.controller.input_vel += int(rs_x * 10)
-        # elif rs_x < 0:
+        # elif rs_x < 0 and odrv1:
         #     odrv0.axis0.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
         #     odrv0.axis1.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
         #     odrv0.axis0.controller.input_vel += int(rs_x * 10)
         #     odrv0.axis1.controller.input_vel += int(rs_x * 10)
 
+# ====================================================================================================
         # if l2 > 0:
         #     print("L2")
         # if r2 > 0:

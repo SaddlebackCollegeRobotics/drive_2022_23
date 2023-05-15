e10ecb5dd32f395ea42feddb927e9e461e8fc7b7
Supernova1114 <supernovawarriors128@gmail.com>
2023-05-15 15:25:18 -0700

    Comment out calibration node from launch

--- drive/launch/rover_drive.launch.py
+++ drive/launch/rover_drive.launch.py
@@ -19,11 +19,11 @@ def generate_launch_description():
         output='screen',
     )
 
-    calibration_node = Node(
-        package='drive',
-        executable='calibration',
-        output='screen',
-    )
+    # calibration_node = Node(
+    #     package='drive',
+    #     executable='calibration',
+    #     output='screen',
+    # )
 
     motor_driver = Node(
         package='drive',
@@ -33,6 +33,6 @@ def generate_launch_description():
 
     return LaunchDescription([
         diff_drive_conversion,
-        calibration_node,
+        # calibration_node,
         motor_driver,
     ])
